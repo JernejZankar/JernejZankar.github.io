fe0d9f3b81702802c05567119c34b037a152e888
JernejZankar <59575609+JernejZankar@users.noreply.github.com>
2020-01-15 12:22:55 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,21 @@
+
+
+function mojaFunkcija() {
+	document.getElementById("demo").innerHTML = "1";
+	
+}
+
+function mojaFunkcija2() {
+	document.getElementById("demo").innerHTML = "2";
+}
+
+function mojaFunkcija3() {
+	document.getElementById("demo").innerHTML = "3";
+}
+function mojaFunkcija4() {
+	document.getElementById("demo").innerHTML = "4";
+}
+
+function mojaFunkcija5() {
+document.getElementById("demo").innerHTML = "5";
+}
